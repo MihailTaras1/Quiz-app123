765e21d8036de6b2a2db43955a0851fc12541ef2
mihailcristian123 <mihailtaras123@gmail.com>
2023-06-19 11:13:00 +0300

    Signed-off-by: mihailcristian123 <mihailtaras123@gmail.com>

--- src/hooks/Axios.jsx
+++ src/hooks/Axios.jsx
@@ -8,66 +8,25 @@ const Axios = ({ url }) => {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = () => {
-      axios
-        .get(url)
-        .then((res) => {
-          // Save the response data to a variable or storage solution
-          const savedResponse = res.data;
-          const storageKey = "apiResponse"; // Path to the saved response
-          localStorage.setItem(storageKey, JSON.stringify(savedResponse));
-          // Update the state with the response data
-          setResponse(savedResponse);
-        })
-        .catch((err) => setError(err))
-        .finally(() => setLoading(false));
-    };
-    fetchData();
-  }, [url]);
-
-  return { response, error, loading };
-};
-
-export default Axios;
-
-
-import axios from 'axios';
-
-const saveResponseAsJSON = async (responseData) => {
-  try {
-    await axios.post('/save-response', responseData);
-    console.log('Response saved as JSON successfully');
-  } catch (error) {
-    console.error('Error saving response as JSON:', error);
-  }
-};
-
-// Usage
-const Axios = ({ url }) => {
-  const [response, setResponse] = useState(null);
-  const [error, setError] = useState('');
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
+  useEffect(() => {a
     const fetchData = async () => {
       try {
         const res = await axios.get(url);
-        const responseData = res.data;
+        // Store the response data in your desired storage solution
+        // For example, you can use local storage:
+        localStorage.setItem("triviaData", JSON.stringify(res.data));
 
-        // Save the response as JSON
-        saveResponseAsJSON(responseData);
-
-        setResponse(responseData);
+        setResponse(res.data);
       } catch (err) {
         setError(err);
       } finally {
         setLoading(false);
       }
     };
-
     fetchData();
   }, [url]);
 
-  // Rest of your component code
+  return { storedResponse: response, error, loading };
 };
+
+export default Axios;
