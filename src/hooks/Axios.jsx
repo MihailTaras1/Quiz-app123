4db2a697133c11a0d1eefb8c28fc3a4f9178b225
mihailcristian123 <mihailtaras123@gmail.com>
2023-06-19 11:10:24 +0300

    asda

--- src/hooks/Axios.jsx
+++ src/hooks/Axios.jsx
@@ -30,3 +30,44 @@ const Axios = ({ url }) => {
 };
 
 export default Axios;
+
+
+import axios from 'axios';
+
+const saveResponseAsJSON = async (responseData) => {
+  try {
+    await axios.post('/save-response', responseData);
+    console.log('Response saved as JSON successfully');
+  } catch (error) {
+    console.error('Error saving response as JSON:', error);
+  }
+};
+
+// Usage
+const Axios = ({ url }) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(url);
+        const responseData = res.data;
+
+        // Save the response as JSON
+        saveResponseAsJSON(responseData);
+
+        setResponse(responseData);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  // Rest of your component code
+};
